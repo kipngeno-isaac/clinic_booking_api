7dc426da95e17f958b4225009de6a9b7db641a61
Isaac <isaac.kipngeno@atlancis.com>
2026-07-15 13:03:20 +0300

    feat: add Patient model and migration for patients table

--- alembic/env.py
+++ alembic/env.py
@@ -8,6 +8,7 @@ from alembic import context
 from app.core.config import settings
 from app.db.base import Base
 from app.models.doctor import Doctor  # noqa: F401
+from app.models.patient import Patient  # noqa: F401
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
